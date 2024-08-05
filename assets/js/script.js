a456b9acb2eb9497e19f20ec037f389224f7fa46
Lauren Yeung <laurenyeung217@gmail.com>
2024-08-05 21:39:00 +0000

    Fetch quote function added.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,9 @@
+const api_url = "https://type.fit/api/quotes"
 
+async function getquote(url){
+    const response = await fetch(url);
+    var data = await response.json();
+    console.log(data)
+}
 
+getquote(api_url)
